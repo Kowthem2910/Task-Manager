36ab596c10e18565a016d5a94bba8d4507d115e7
Kowthem2910 <kowthem3@gmail.com>
2024-02-11 13:32:23 +0530

    redux restore

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,31 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home, SignUp, Login } from "./pages/Index";
 import AuthRoutes from "./Routes/AuthRoutes";
-import store from './Redux/reducer';
+import store , {persistor}from "./Redux/reducer";
 import { Toaster } from "./components/ui/toaster";
+import { PersistGate } from "redux-persist/integration/react";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
-        <Routes>
-        <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route
-            path="/home"
-            element={
-              <AuthRoutes>
-                <Home />
-              </AuthRoutes>
-            }
-          />
-        </Routes>
-      </Router>
-      <Toaster/>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route
+              path="/home"
+              element={
+                <AuthRoutes>
+                  <Home />
+                </AuthRoutes>
+              }
+            />
+          </Routes>
+        </Router>
+        <Toaster />
+      </PersistGate>
     </Provider>
   );
 };
--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,10 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 import userReducer from "./store";
+
+
+const persistConfig = {
+  key:'root',
+  storage
+}
+
+
+const persistedReducer = persistReducer(persistConfig, userReducer)
 
 const store = configureStore({
   reducer: {
-    user: userReducer
-  }
+    user:persistedReducer,
+  },
 });
 
+export const persistor = persistStore(store); 
 export default store;
